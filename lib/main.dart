6d04aa61e028809952fa739cfc8603f23f0dfea8
Javier Pinzon <javierpinzon804@gmail.com>
2025-06-27 13:20:06 -0500

    optimizadas

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Reserva de Canchas',
+      title: 'Reserva de Canchas.   mira, estoy aciendo un proyecto en flutter y ya va a pasar a produccion. mi proyecto esta vinculado a firebase, te voy a pasar mi codigo uno por uno con sus advertencias y me las solucionaras, quiero que tambien me optimices los proceso de firebase y que quede funcionando de la forma mas optima posible todo. no quiero que cambies la logica ni la funcionalidad actual, solo solucionar las advertencias sin dañar mi codigo y si es posible optimizar el proceso de firebase en colsultas y permisos y todo lo relacionado a firebase ya que el cobra por uso, pero no quiero que me lo reestructures, solo optimiza si puedes, sino puedes, no lo hagas, es que tengo muchoos archivos dependientes de otros, por eso no puedo cambiar mucho los datos.pero no quiero que uses cache porque eso aveces influye con la actualizacion o visualizacion de datos en tiempo real, lo que no es recomendable en una app de reservas. quiero que se optimise con el firebase pero dentro de lo nnormal, me entiendes? vamos con el primer codigo. y con optimizar el codigo para firebase me refiero a ver que todo vaya bien, que no hayan redundancias y que no se hagan consultas innecesarias, me entiendes? vamos con el primer codigo.',
       debugShowCheckedModeBanner: false,
       navigatorObservers: [routeObserver],
       theme: ThemeData(primaryColor: Colors.green),
@@ -44,3 +44,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
